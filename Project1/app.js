12d07172e9e10300fc1c785f9a8a78f8070d5719
Craig Scott <craigtscottjr@gmail.com>
2017-11-27 18:55:52 -0800

    Project1: refactor code

--- Project1/app.js
+++ Project1/app.js
@@ -1,1 +1,11 @@
-console.log("hi");
+var http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 3000;
+var server = http.createServer((request, responce) => {
+  responce.statusCode = 200;
+  responce.setHeadder('Content-type', 'text/plain');
+  responce.end('hello world!')
+})
+
+server.listen(port);
